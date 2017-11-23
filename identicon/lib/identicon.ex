00b1a3abe1624d342824cbaf3baf30ba9ccd7d14
AlexanderHarley <moh_Harper@msn.com>
2017-11-23 11:02:33 +0000

    Completed Identicon project

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,10 @@ defmodule Identicon do
         |> hash_input
         |> pick_colour
         |> build_grid
+        |> filter_odd_squares
+        |> build_pixel_map
+        |> draw_image
+        |> save_image(input)
     end
 
     def hash_input(input) do
@@ -18,9 +22,17 @@ defmodule Identicon do
     end
 
     def build_grid(%Identicon.Image{hex: hex} = image) do
-        hex
-        |> Enum.chunk(3)
-        # |> mirror_row
+        grid =
+            hex
+            |> Enum.chunk(3)
+            # Enum.map(mirror_row) will pass the return value of the function to .map, causing an error
+            # Enum.map(&mirror_row/1) will pass the reference to the function to the .map, working as intended
+            # The /1 represents the arity of the function
+            |> Enum.map(&mirror_row/1)
+            |> List.flatten
+            |> Enum.with_index
+
+        %Identicon.Image{image | grid: grid}
     end
 
     def mirror_row(row) do
@@ -29,5 +41,41 @@ defmodule Identicon do
 
         # [145, 46, 200, 46, 145]
         row ++ [second, first]
+    end
+
+    def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+        grid = Enum.filter grid, fn({value, _}) ->
+            rem(value, 2) == 0
+        end
+
+        %Identicon.Image{image | grid: grid}
+    end
+
+    def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+        pixel_map = Enum.map grid, fn({_, index}) ->
+            x = rem(index, 5) * 50
+            y = div(index, 5) * 50
+            top_left     = {x, y}
+            bottom_right = {x + 50, y + 50}
+
+            {top_left, bottom_right}
+        end
+
+        %Identicon.Image{image | pixel_map: pixel_map}
+    end
+
+    def draw_image(%Identicon.Image{colour: colour, pixel_map: pixel_map}) do
+        image = :egd.create(250, 250)
+        fill  = :egd.color(colour)
+
+        Enum.each pixel_map, fn({start, stop}) ->
+            :egd.filledRectangle(image, start, stop, fill)
+        end
+
+        :egd.render(image)
+    end
+
+    def save_image(image, filename) do
+        File.write("#{filename}.png", image)
     end
 end
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -1,4 +1,4 @@
 defmodule Identicon.Image do
     # `defstruct hex: nil` is equivalent to `defstruct [hex: nil]`
-    defstruct hex: nil, colour: nil
+    defstruct hex: nil, colour: nil, grid: nil, pixel_map: nil
 end
